6d746cb113d51343912b1f6632dba43547190189
MustafaAkagunduz <akagunduzmustafa00@gmail.com>
2022-08-30 17:08:45 +0300

    update: 30.08.2022 17:08

--- src/chapters/Chapter3/C3_E16.java
+++ src/chapters/Chapter3/C3_E16.java
@@ -6,23 +6,22 @@ public class C3_E16 {
 
 	public static void main(String[] args) {
 
-        Random random = new Random();
+		Random r= new Random();
+		
+        int isXnegative = r.nextInt(2); // if "1" comes from here; I will convert the random number to negative for x 
+        int isYnegative = r.nextInt(2); // if "1" comes from here; I will convert the random number to negative for y 
+        
+        System.out.println(isXnegative);
+        System.out.println(isYnegative);
+        int x = (int) ((Math.random() * 50));
+        int y = (int) ((Math.random() * 100));
 
-        //Random boolean value indicates making the number negative.
-        boolean negOrPosX = random.nextBoolean();
-        boolean negOrPosY = random.nextBoolean();
 
-        int x = (int) ((Math.random() * 100));
-        int y = (int) ((Math.random() * 200));
+        if (isXnegative==1) { x = x * -1;
+        } 
 
-
-        if (negOrPosX) {
-            x = x * -1;
-        }
-
-        if (negOrPosY) {
-            y = y * -1;
-        }
+        if (isYnegative==1) { y = y * -1;
+        } 
 
         System.out.println("(" + x + "," + y + ")");
     }
--- src/chapters/Chapter3/C3_E27.java
+++ src/chapters/Chapter3/C3_E27.java
@@ -1,14 +1,34 @@
 package chapters.Chapter3;
 
+//import java.util.Scanner;
+
 public class C3_E27 {
 
 	public static void main(String[] args) {
 
-
 		/* ANSWER OF THIS QUESTION IS MISSING BECAUSE OF MY LACK OF GEOMETRY KNOWLEDGE
 		   PLEASE EXCUSE ME FOR THIS.. */
 		
+		/*Scanner scn = new Scanner(System.in);
+		System.out.println(
+				"We have a right triangle(first zone triangle), its axis-x point is:200 and axis-y point is:100");
+		System.out.println("Please enter a point and we'll check whether it is in the triangle: ");
+		System.out.println("Please enter the x: ");
+		int x = scn.nextInt();
+		System.out.println("Please enter the y: ");
+		int y = scn.nextInt();
 
+		if (((x >= 100) && (x<=200)) && (y <= 50 && ) {
+			System.out.println("Your point is in the triangle..");
+		} else if ((x <= 100 && x>=0) && (y >= 50 && y<=100)) {
+			System.out.println("Your point is in the triangle..");
+
+		} else if ((((x <= 100 && x >= 0) && (y <= 50 && y >= 0)))) {
+			System.out.println("Your point is in the triangle..");
+		} else
+			System.out.println("Your point is at the outside of the triangle...");
+
+		scn.close();*/
 	}
 
 }
